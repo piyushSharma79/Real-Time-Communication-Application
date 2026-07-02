94bae1761af99af0e6d1820c38b9249aa5b4c68a
Piyush Sharma <ps1375982@gmail.com>
2026-07-03 00:17:20 +0530

    Implement JWT generation and validation service

--- src/main/java/com/backend/realtimeapp_backend/services/JwtService.java
+++ src/main/java/com/backend/realtimeapp_backend/services/JwtService.java
@@ -1,10 +1,11 @@
 package com.backend.realtimeapp_backend.services;
 
+import com.backend.realtimeapp_backend.entities.Role;
 import com.backend.realtimeapp_backend.entities.User;
 
 public interface JwtService {
-    String generateToken(User user);
-    Boolean validateToken(String token, User user);
+    String generateToken(String email, Role role);
+    boolean validateToken(String token, String email);
     String extractEmail(String token);
-    Boolean isTokenExpired(String token);
+    boolean isTokenExpired(String token);
 }
--- src/main/java/com/backend/realtimeapp_backend/services/JwtServiceImpl.java
+++ src/main/java/com/backend/realtimeapp_backend/services/JwtServiceImpl.java
@@ -1,28 +1,66 @@
 package com.backend.realtimeapp_backend.services;
 
-import com.backend.realtimeapp_backend.entities.User;
+import com.backend.realtimeapp_backend.entities.Role;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class JwtServiceImpl implements JwtService {
 
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private long expiration;
+
+    private SecretKey getSigningKey(){
+        return Keys.hmacShaKeyFor(
+                secret.getBytes(StandardCharsets.UTF_8)
+        );
+    }
     @Override
-    public String generateToken(User user) {
-        return "";
+    public String generateToken(String email, Role role) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("role", role.name());
+
+        return Jwts.builder()
+                .subject(email)
+                .claims(claims)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+    public String extractEmail(String token){
+        return extractAllClaims(token).getSubject();
     }
 
     @Override
-    public Boolean validateToken(String token, User user) {
-        return null;
+    public boolean isTokenExpired(String token) {
+        return extractAllClaims(token)
+                .getExpiration()
+                .before(new Date()); //is this the expiration time of token before the current time
     }
 
     @Override
-    public String extractEmail(String token) {
-        return "";
-    }
-
-    @Override
-    public Boolean isTokenExpired(String token) {
-        return null;
+    public boolean validateToken(String token, String email) {
+        return extractEmail(token).equals(email) && !isTokenExpired(token);
     }
 }
